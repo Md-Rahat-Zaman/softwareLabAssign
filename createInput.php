777a35811da29869f814f7df2358d6437f53bd1c
unknown <you@example.com>
2022-05-14 12:17:30 +0600

    again commit

--- createInput.php
+++ createInput.php
@@ -1,28 +1,32 @@
 <h1>Create Record</h1>
 
-
+<img style="position: relative; left: 390px;" src="student.png">
 
 <form method=get action=createReasult.php>
+    
+	<p style="color:blue">
 
-	<p>
-
-	user_id: <input type = int name = user_id> <br>
+	user_id: <input style="border-color:  #ADD8E6;" type = int name = user_id> <br>
 	</p>
-	user_name: <input type=text name=user_name> <br>
-
-	<p>
-     user_pass: <input type = int name = user_pass> <br>
-	 
-	 <p>
-
-	 email: <input type = text name = email><br>
-	 <p>
-		Universityuniversity_id: <input type = int name = Universityuniversity_id> <br>
+	<p style="color:blue">
+	user_name: <input style="border-color:  #ADD8E6;" type=text name=user_name> <br>
+	</p>
+	<p style="color:blue">
+     user_pass: <input style="border-color:  #ADD8E6;" type = int name = user_pass> <br>
 	 </p>
-	 <p>
-		 approval: <input type = binary name = approval> <br>
+	 <p style="color:blue">
+
+	 email: <input style="border-color:  #ADD8E6;" type = text name = email><br>
+	</p>
+	 <p style="color:blue">
+		Universityuniversity_id: <input style="border-color:  #ADD8E6;" type = int name = Universityuniversity_id> <br>
 	 </p>
-	 status: <input type = binary name = status> <br>
-	<input type=submit value=Insert>
+	 <p style="color:blue">
+		 approval: <input style="border-color:  #ADD8E6;" type = binary name = approval> <br>
+	 </p>
+	 <p style="color:blue">
+	 status: <input style="border-color:  #ADD8E6;" type = binary name = status> <br>
+	</p>
+	<input  style="border-color:  #ADD8E6;position: relative; bottom: -30px; left: 80px;" type=submit value=Insert>
 
 </form>
